35e32801d86662f621eed1e3e7752dcae0c146fe
Nnenna Okibe <nnennaokibe@gmail.com>
2025-05-20 13:34:26 +0100

    update

--- Selenium_Test.py
+++ Selenium_Test.py
@@ -10,32 +10,32 @@ time.sleep(5)
 
 #Log in page
 driver.find_element(By.ID, "email-id").send_keys("confixhub@yopmail.com")#Email text field
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, "password").send_keys("Qwerty")#Password text field
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, "remember").click()#Click Remember me Button
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, "submit-id").click()#Click Submit Button
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, "new-customer").click()#Click Add Customer button on the landing page
-time.sleep(5)
+time.sleep(2)
 
 #Add Customer Form
 driver.find_element(By.ID, "EmailAddress").send_keys("johndoe@yopmail.com")# input valid Email Address
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, "FirstName").send_keys("John")#Input FirstName
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, "LastName").send_keys("Doe")#input LastName
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, "City").send_keys("Enugu")#input City
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, "StateOrRegion").send_keys("Iowa")# Select an option from the 'State' drop-down
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.CSS_SELECTOR, "input[name='gender'][value='male']").click() #Select 'gender' button
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.CSS_SELECTOR, "input[name='promos-name']").click() # Click on "Add to promotional list" Button
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.CLASS_NAME, "btn-primary").click()# Click 'Submit' button
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.CLASS_NAME, "nav-link").click()# Click 'Sign out' button
-time.sleep(5)
+time.sleep(2)
